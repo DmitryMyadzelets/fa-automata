540f344992b7e840bd3ce5c71b0347e8c42efb7b
Dmitry Myadzelets <DmitryMyadzelets@gmail.com>
2014-09-22 22:55:17 +0200

    Refactoring step 1 is done

--- js/editor/start.js
+++ js/editor/start.js
@@ -2,3 +2,15 @@
 
 var ed = { version: "1.0.0" };
 
+//
+// This module implements an user interface for interacting with 
+// an automata graph.
+// 
+
+// Look at some examples:
+// http://bl.ocks.org/mbostock/4600693
+// http://bl.ocks.org/MoritzStefaner/1377729
+// http://bl.ocks.org/rkirsling/5001347
+// http://bl.ocks.org/benzguo/4370043
+// http://tutorials.jenkov.com/svg/svg-and-css.html // SVG and CSS
+
