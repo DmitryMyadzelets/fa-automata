d48b4c7fa1c90a0a1e9095019977110c852582f4
Dmitry Myadzelets <DmitryMyadzelets@gmail.com>
2015-02-21 00:01:50 +0100

    Cleanup

--- js/editor/undo.js
+++ js/editor/undo.js
@@ -4,192 +4,178 @@
 "use strict";
 
 
-// 'Command' class
-// 
-var Command = function (redo, undo) {
-    if (redo) { this.redo = redo; }
-    if (undo) { this.undo = undo; }
-};
 
-Command.prototype.redo = function () {};
-Command.prototype.undo = function () {};
+var Commands = (function () {
 
+    var Command = function (redo, undo) {
+        if (redo) { this.redo = redo; }
+        if (undo) { this.undo = undo; }
+    };
 
+    function dummy() { return; }
 
-// 'Commands' object
-// 
-var commands = {
-    stack : [],
-    macro : [],
-    // Index is equal to a number of commands which the user can undo;
-    // If index is not equal to the length of stack, it implies
-    // that user did "undo". Then new command cancels all the
-    // values in stack above the index.
-    index : 0,
-    on : {}
-};
+    Command.prototype.redo = dummy;
+    Command.prototype.undo = dummy;
 
 
-function commands_methods() {
+    function prototype_methods() {
 
-    function on_event() {
-        var fun = this.on.update;
-        if (typeof fun === 'function') {
-            fun();
+        // Starts new macro recording
+        this.start = function () {
+            if (this.index < this.stack.length) { this.stack.length = this.index; }
+            this.macro = [];
+            this.stack.push(this.macro);
+            this.index = this.stack.length;
+            return this;
+        };
+
+        this.undo = function () {
+            if (this.index > 0) {
+                var macro = this.stack[--this.index];
+                var i = macro.length;
+                while (i-- > 0) {
+                    macro[i].undo();
+                }
+                this.update();
+            }
+        };
+
+        this.redo = function () {
+            if (this.index < this.stack.length) {
+                var macro = this.stack[this.index++];
+                var i, n = macro.length;
+                for (i = 0; i < n; i++) {
+                    macro[i].redo();
+                }
+                this.update();
+            }
+        };
+
+        // Makes a copy of each item in arguments if it is an array
+        function copy_arguments(args) {
+            var i = args.length;
+            while (i--) {
+                if (args[i] instanceof Array) {
+                    args[i] = args[i].slice(0);
+                }
+            }
         }
+
+        // Creates new command-function as the key of a 'Command' instance
+        this.new = function (name, fun) {
+            if (this[name] && console) {
+                console.error('Command', name, 'already exists');
+                return;
+            }
+            var self = this;
+            if (name && typeof fun === 'function') {
+                this[name] = function () {
+                    var command = new Command();
+                    copy_arguments(arguments);
+                    fun.apply(command, arguments);
+                    self.macro.push(command);
+                    command.redo();
+                    this.update();
+                    return self;
+                };
+            }
+        };
+
+        this.clean = function () {
+            this.stack.length = 0;
+            this.macro.length = 0;
+        };
     }
 
-    // Starts new macro recording
-    this.start = function () {
-        if (this.index < this.stack.length) { this.stack.length = this.index; }
+
+    var instance = function () {
+        this.stack = [];
         this.macro = [];
-        this.stack.push(this.macro);
-        this.index = this.stack.length;
-        return this;
+        // Index is equal to a number of commands which the user can undo;
+        // If index is not equal to the length of stack, it implies
+        // that user did "undo". Then new command cancels all the
+        // values in stack above the index.
+        this.index = 0;
+        this.update = dummy;
     };
 
+    prototype_methods.call(instance.prototype);
+    return instance;
 
-    this.undo = function () {
-        if (this.index > 0) {
-            var macro = this.stack[--this.index];
-            var i = macro.length;
-            while (i-- > 0) {
-                macro[i].undo();
-            }
-            on_event.call(this);
-        }
-    };
-
-
-    this.redo = function () {
-        if (this.index < this.stack.length) {
-            var macro = this.stack[this.index++];
-            var i, n = macro.length;
-            for (i = 0; i < n; i++) {
-                macro[i].redo();
-            }
-            on_event.call(this);
-        }
-    };
-
-
-    // Makes a copy of each item in arguments if it is an array
-    function copy_arguments(arguments) {
-        var i = arguments.length;
-        while (i--) {
-            if (arguments[i] instanceof Array) {
-                arguments[i] = arguments[i].slice(0);
-            }
-        }
-    }
-
-
-    // Creates new command-function as the key of a 'Commands' instance, i.e. it will be
-    // var command = new Commands();
-    // ...
-    // command
-    this.new = function (name, fun) {
-        if (this[name] && console) {
-            console.error('Command', name, 'already exists');
-            return;
-        }
-        var self = this;
-        if (name && typeof fun === 'function') {
-            this[name] = function () {
-                var command = new Command();
-                copy_arguments(arguments);
-                fun.apply(command, arguments);
-                self.macro.push(command);
-                command.redo();
-                on_event.call(this);
-                return self;
-            };
-        }
-    };
-}
+}());
 
 
 
-commands_methods.call(commands);
-
-// Makes a copy of each item in arguments if it is an array
-function copy_arguments() {
-    var i = arguments.length;
-    while (i--) {
-        if (arguments[i] instanceof Array) {
-            arguments[i] = arguments[i].slice(0);
-        }
-    }
-}
+var commands = new Commands();
 
 
-commands.new('add_node', function (model, d) {
-    this.redo = function () { model.node.add(d); };
-    this.undo = function () { model.node.remove(d); };
+commands.new('add_node', function (graph, d) {
+    this.redo = function () { graph.node.add(d); };
+    this.undo = function () { graph.node.remove(d); };
 });
 
 
-commands.new('del_node', function (model, d) {
-    this.redo = function () { model.node.remove(d); };
-    this.undo = function () { model.node.add(d); };
+commands.new('del_node', function (graph, d) {
+    this.redo = function () { graph.node.remove(d); };
+    this.undo = function () { graph.node.add(d); };
 });
 
 
-commands.new('add_edge', function (model, d) {
-    this.redo = function () { model.edge.add(d); };
-    this.undo = function () { model.edge.remove(d); };
+commands.new('add_edge', function (graph, d) {
+    this.redo = function () { graph.edge.add(d); };
+    this.undo = function () { graph.edge.remove(d); };
 });
 
 
-commands.new('del_edge', function (model, d) {
-    this.redo = function () { model.edge.remove(d); };
-    this.undo = function () { model.edge.add(d); };
+commands.new('del_edge', function (graph, d) {
+    this.redo = function () { graph.edge.remove(d); };
+    this.undo = function () { graph.edge.add(d); };
 });
 
 
-commands.new('node_text', function (model, d, text) {
+commands.new('node_text', function (graph, d, text) {
     var old_text = d.text;
-    this.redo = function () { model.node.text(d, text); };
-    this.undo = function () { model.node.text(d, old_text); };
+    this.redo = function () { graph.node.text(d, text); };
+    this.undo = function () { graph.node.text(d, old_text); };
 });
 
-commands.new('edge_text', function (model, d, text) {
+commands.new('edge_text', function (graph, d, text) {
     var old_text = d.text;
-    this.redo = function () { model.edge.text(d, text); };
-    this.undo = function () { model.edge.text(d, old_text); };
+    this.redo = function () { graph.edge.text(d, text); };
+    this.undo = function () { graph.edge.text(d, old_text); };
 });
 
-commands.new('move_node', function (model, d, from, to) {
-    this.redo = function () { model.node.move(d, to); };
-    this.undo = function () { model.node.move(d, from); };
+commands.new('move_node', function (graph, d, from, to) {
+    this.redo = function () { graph.node.move(d, to); };
+    this.undo = function () { graph.node.move(d, from); };
 });
 
-commands.new('mark_node', function (model, d) {
-    this.redo = function () { model.node.mark(d); };
-    this.undo = function () { model.node.unmark(d); };
+commands.new('mark_node', function (graph, d) {
+    this.redo = function () { graph.node.mark(d); };
+    this.undo = function () { graph.node.unmark(d); };
 });
 
-commands.new('unmark_node', function (model, d) {
-    this.redo = function () { model.node.unmark(d); };
-    this.undo = function () { model.node.mark(d); };
+commands.new('unmark_node', function (graph, d) {
+    this.redo = function () { graph.node.unmark(d); };
+    this.undo = function () { graph.node.mark(d); };
 });
 
-commands.new('initial', function (model, from, to) {
-    this.redo = function () { model.node.initial(to); };
-    this.undo = function () { model.node.initial(from); };
+commands.new('initial', function (graph, from, to) {
+    this.redo = function () { graph.node.initial(to); };
+    this.undo = function () { graph.node.initial(from); };
 });
 
-commands.new('move_edge', function (model, d, from, to) {
-    this.redo = function () { model.edge.move(d, to[0], to[1]); };
-    this.undo = function () { model.edge.move(d, from[0], from[1]); };
+commands.new('move_edge', function (graph, d, from, to) {
+    this.redo = function () { graph.edge.move(d, to[0], to[1]); };
+    this.undo = function () { graph.edge.move(d, from[0], from[1]); };
 });
 
-commands.new('spring', function (view, model) {
+commands.new('spring', function (view, graph) {
     var xy = [];
-    var nodes =  model.object().nodes;
+    var nodes =  graph.object().nodes;
     nodes.forEach(function (d) { xy.push(d.x, d.y); });
     this.redo = function () { view.spring(true); };
-    this.undo = function () { view.spring(false); model.node.move(nodes, xy); };
+    this.undo = function () { view.spring(false); graph.node.move(nodes, xy); };
 });
 
 
